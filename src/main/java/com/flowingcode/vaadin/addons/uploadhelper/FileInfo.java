7ff6ceed142fc3f494d90770c9d99c1a14b36952
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2026-02-26 11:26:54 -0300

    feat: add support for Vaadin 25
    Close #5

--- src/main/java/com/flowingcode/vaadin/addons/uploadhelper/FileInfo.java
+++ src/main/java/com/flowingcode/vaadin/addons/uploadhelper/FileInfo.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,7 @@
  */
 package com.flowingcode.vaadin.addons.uploadhelper;
 
+import com.flowingcode.vaadin.jsonmigration.JsonMigration;
 import com.vaadin.flow.component.upload.FinishedEvent;
 import com.vaadin.flow.component.upload.Upload;
 import elemental.json.Json;
@@ -26,9 +27,11 @@ import elemental.json.JsonObject;
 import java.io.Serializable;
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.experimental.ExtensionMethod;
 
 /** Represents a file in the file list of an {@link Upload} component. */
 @SuppressWarnings("serial")
+@ExtensionMethod(value = JsonMigration.class, suppressBaseMethods = true)
 public final class FileInfo implements Serializable {
 
   private final String name;
